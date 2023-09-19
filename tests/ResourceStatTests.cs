298cf644973629fb5a7817a011c27050f40d10f9
Greenstack <paradoxbuilder@gmail.com>
2023-09-18 22:05:04 -0600

    Added tests for depleting and replenishing

--- tests/ResourceStatTests.cs
+++ tests/ResourceStatTests.cs
@@ -2,11 +2,6 @@
 
 public class ResourceStatTests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
     public void TestIncrease()
     {
@@ -40,5 +35,33 @@ public class ResourceStatTests
 
         stat.Reduce(10);
         Assert.That(stat.CurrentValue, Is.EqualTo(stat.Min));
+    }
+
+    [Test]
+    public void TestDeplete()
+    {
+        ResourceStat<int> stat = new()
+        {
+            CurrentValue = 10,
+            Max = 20,
+            Min = 0,
+        };
+
+        stat.Deplete();
+        Assert.That(stat.CurrentValue, Is.EqualTo(stat.Min));
+    }
+
+    [Test]
+    public void TestReplenish()
+    {
+        ResourceStat<int> stat = new()
+        {
+            CurrentValue = 10,
+            Max = 20,
+            Min = 0,
+        };
+
+        stat.RestoreInFull();
+        Assert.That(stat.CurrentValue, Is.EqualTo(stat.Max));
     }
 }
